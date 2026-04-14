c54cf778e689e002322729fde56acdfc44251bca
animexgaemr1 <animexgamer9@gmail.com>
2026-04-14 17:20:46 +0800

    prefinal exam

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style ={styles.text}> 
+        Mark Rafael C. Sabulao  2ndYear  BSIT2B
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 30,
+    textAlign: 'center',
+  }
+});
