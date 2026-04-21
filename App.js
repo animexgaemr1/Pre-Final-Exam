f7ae1d6140aa91f60734ab5eed2535c8cedb52dc
xhandrelrdeveramango11-star <aldrexluxford@gmail.com>
2026-04-21 18:28:54 +0800

    New file added

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style ={styles.text}> 
-        Mark Rafael C. Sabulao  2ndYear  BSIT2B
+        Ivance Salicsic
       </Text>
       <StatusBar style="auto" />
     </View>
